db795f265b2f712b48f42a04545d41a720081945
Christoph Bier <christoph.bier@gmx.de>
2024-01-09 16:29:10 +0100

    Fix solarized colors in Diffview

--- lua/chribi/colors.lua
+++ lua/chribi/colors.lua
@@ -1,20 +1,25 @@
 local colors = require('solarized.colors')
 
-local function adjust_colors()
+local function adjust_colors_solarized()
     vim.api.nvim_set_hl(0, 'qfLineNr', { fg = 'fg', italic = true })
     vim.api.nvim_set_hl(0, 'Pmenu', { link = 'NormalFloat' })
     vim.api.nvim_set_hl(0, 'Search', { ctermfg = 0, ctermbg = 225, fg = colors.white, bg = colors.fg_light })
     vim.api.nvim_set_hl(0, 'IncSearch', { fg = colors.white, bg = colors.blue })
     vim.api.nvim_set_hl(0, 'QuickFixLine', { link = 'Search' })
     vim.api.nvim_set_hl(0, 'NonText', { fg = colors.magenta })
+
+    vim.api.nvim_set_hl(0, 'DiffDelete', { bg = "none", fg = colors.red })
+    vim.api.nvim_set_hl(0, 'DiffviewDiffDelete', { bg = "none", fg = colors.red })
+    vim.api.nvim_set_hl(0, 'DiffAdd', { bg = '#CDD998' })
+    vim.api.nvim_set_hl(0, 'DiffChange', { bg = '#FFE8BE' })
 end
 
-adjust_colors()
+adjust_colors_solarized()
 
 local colors_group = vim.api.nvim_create_augroup('AdjustColors', { clear = true })
 
 vim.api.nvim_create_autocmd('ColorScheme', {
-    pattern = '*',
+    pattern = 'solarized',
     group = colors_group,
-    callback = adjust_colors,
+    callback = adjust_colors_solarized,
 })
